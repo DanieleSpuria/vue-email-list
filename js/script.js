824709e383cccbf04171cc45ae6b244a5be27dbe
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-04-21 14:39:16 +0200

    add axios

--- js/script.js
+++ js/script.js
@@ -3,15 +3,20 @@ const {createApp} = Vue;
 createApp({
   data(){
     return{
-      
+     url: 'https://flynn.boolean.careers/exercises/api/random/mail' 
     }
   },
 
   methods: {
-    
+    api() {
+      axios.get(url)
+      .then (result => {
+        console.log(result.data);
+      })
+    }
   },
 
   mounted() {
-    
+    api()
   }
 }).mount('#app')
